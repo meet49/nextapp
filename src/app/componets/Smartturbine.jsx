f68b8cdff9fe79e2f8011128fbda04272e6c0af2
meet49 <meetsojitra49@gmail.com>
2024-04-26 10:49:32 +0530

    enrgy  app using next js

--- src/app/componets/Smartturbine.jsx
+++ src/app/componets/Smartturbine.jsx
@@ -20,12 +20,12 @@ function Smartturbine() {
           industry.
         </p>
       </div>
-      <div className="flex xl:flex-row flex-col  justify-center items-center gap-20 relative sm:mt-5   animate-myfirst"> 
+      <div className="flex xl:flex-row flex-col  justify-center items-center gap-20 relative    animate-myfirst"> 
         <div className="flex flex-col gap-8 pt-2 sm:pt-10  animate-myfirst">
           <div className="bg-white inline-flex rounded-lg px-10 z-10 ">
             <Image src={s2} width={"94"} height={"94"} alt=""/>
             <div>
-              <p className="text-[#1F1F1F] font-bold text-[52px]">110+</p>
+              <p className="text-[#1F1F1F] font-bold sm:text-[52px] text-[25px]">110+</p>
               <p className="text-[#898989] font-medium text-[18px]">
                 Project Completed
               </p>
@@ -34,21 +34,21 @@ function Smartturbine() {
           <div className="bg-white inline-flex rounded-lg px-10 z-10 ">
             <Image src={s3} width={"94"} height={"94"} alt=""/>
             <div>
-              <p className="text-[#1F1F1F] font-bold text-[52px]">11+</p>
+              <p className="text-[#1F1F1F] font-bold sm:text-[52px] text-[25px]">11+</p>
               <p className="text-[#898989] font-medium text-[18px]">
                 Years Of Experience
               </p>
             </div>
           </div>
         </div>
-        <div className="opacity-25 xl:opacity-100 absolute top-0 xl:relative z-0 animate-spin">
+        <div className="opacity-25 xl:opacity-100 absolute top-0 xl:relative z-0 sm:animate-spin">
           <Image src={fan} alt=""/>
         </div>
         <div className="flex flex-col gap-8 absolute top-[110%] xl:relative">
           <div className="bg-white inline-flex rounded-lg px-10 ">
             <Image src={s4} width={"94"} height={"94"} alt=""/>
             <div>
-              <p className="text-[#1F1F1F] font-bold text-[52px]">90+</p>
+              <p className="text-[#1F1F1F] font-bold sm:text-[52px] text-[25px]">90+</p>
               <p className="text-[#898989] font-medium text-[18px]">
                 Professional Staff
               </p>
@@ -57,7 +57,7 @@ function Smartturbine() {
           <div className="bg-white inline-flex rounded-lg px-10 ">
             <Image src={s5} width={"94"} height={"94"} alt=""/>
             <div>
-              <p className="text-[#1F1F1F] font-bold text-[52px]">170+</p>
+              <p className="text-[#1F1F1F] font-bold sm:text-[52px] text-[25px]">170+</p>
               <p className="text-[#898989] font-medium text-[18px]">
                 Happy Clients
               </p>
--- src/app/componets/Whoweare.jsx
+++ src/app/componets/Whoweare.jsx
@@ -7,41 +7,43 @@ import w4 from "@/app/assets/img/w4.png";
 
 function Whoweare() {
   return (
-    <div className="my-20 sm:ml-16 mb-96 lg:mb-0">
-      <div className="bg-[url('../../public/bg1.png')] bg-no-repeat bg-cover h-fit xl:h-screen flex relative md:p-10">
-        <div className="lg:w-[50%] w-[100%] opacity-15 lg:opacity-100">
-          <Image src={w6} alt=""/>
+    <div className="my-20 sm:ml-16 mb-20 lg:mb-0">
+      <div className="bg-[url('../../public/bg1.png')] bg-no-repeat bg-cover sm:h-fit xl:h-screen flex lg:justify-between relative md:p-10">
+        <div className="lg:w-[50%] w-[80%] left-10 opacity-15 lg:opacity-100 absolute lg:relative">
+          <Image src={w6} />
         </div>
-        <div className="w-[100%] lg:w-[50%] px-[3%] pt-[2%] 2xl:pr-[11%] absolute lg:relative flex flex-col justify-start items-center  lg:items-start
-         ">
-          <div>
-            <Image src={w3}  className="inline pr-2" alt=""/>
-            <p className="font-medium text-2xl inline italic">WHO WE ARE</p>
-          </div>
-          <p className="font-medium text-[52px]">
-            The Best Solution Provider For{" "}
-            <span className="font-bold">Green Energy</span>
-          </p>
-          <p className="text-xl pb-2">
-            We have a team of domain experts who strive for excellence in the
-            renewable energy generation sector...
-          </p>
-          <div className="flex gap-5 ">
+        <div className="w-[100%] lg:w-[50%] px-[3%] pt-[2%] 2xl:pr-[11%]  flex flex-col justify-start items-center lg:items-start">
+          <div className="">
             <div>
-              <Image src={w4} className="lg:animate-spin" alt=""/>
-              <p className="font-semibold text-[28px]">1. Our Culture</p>
-              <p className="text-[#787878] text-lg text-justify px-2">
-                Our passion to work with the client satisfaction is the source
-                of our success. We 100% believe in ethics...
-              </p>
+              <Image src={w3} className="inline pr-2" />
+              <p className="font-medium text-2xl inline italic">WHO WE ARE</p>
             </div>
-            <div className="pt-2">
-              <Image src={w5} className="lg:animate-pulse" alt=""/>
-              <p className="font-semibold text-[28px]">2. Our Productivity</p>
-              <p className="text-[#787878] text-lg text-justify px-5">
-                We take pride in successfully executing 1.30 GW+ and pipelining
-                renewable energy projects of capacity of 1GW in next...
-              </p>
+            <p className="font-medium text-[52px]">
+              The Best Solution Provider For{" "}
+              <span className="font-bold">Green Energy</span>
+            </p>
+            <p className="text-xl pb-2">
+              We have a team of domain experts who strive for excellence in the
+              renewable energy generation sector...
+            </p>
+            <div className="flex gap-5 ">
+              <div>
+                <Image src={w4} className="lg:animate-spin" />
+                <p className="font-semibold text-[28px]">1. Our Culture</p>
+                <p className="text-[#787878] text-lg text-justify px-2">
+                  Our passion to work with the client satisfaction is the source
+                  of our success. We 100% believe in ethics...
+                </p>
+              </div>
+              <div className="pt-2">
+                <Image src={w5} className="lg:animate-pulse" />
+                <p className="font-semibold text-[28px]">2. Our Productivity</p>
+                <p className="text-[#787878] text-lg text-justify px-5">
+                  We take pride in successfully executing 1.30 GW+ and
+                  pipelining renewable energy projects of capacity of 1GW in
+                  next...
+                </p>
+              </div>
             </div>
           </div>
         </div>
